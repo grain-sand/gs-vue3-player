ec5f64f66bd4184316f06ef8339a185a82aefe2f
grain-sand <Grain_Of_Sand@outlook.com>
2026-05-01 21:57:53 +0800

    2.0.0: 实现 GsPlayer.vue 步骤 7 添加包装工具

---
+++ src/util/wrapComponent.ts
@@ -0,0 +1,16 @@
+import {defineComponent, h, type Component, type ComponentPublicInstance} from 'vue';
+
+type ExtractComponentProps<T> = T extends new () => ComponentPublicInstance<infer P>
+  ? P
+  : T extends { props?: infer P }
+    ? P
+    : Record<string, unknown>;
+
+export function wrapComponent<T extends Component>(
+  component: T,
+  props: ExtractComponentProps<T>
+): Component {
+  return defineComponent(() => {
+    return () => h(component, props);
+  });
+}
